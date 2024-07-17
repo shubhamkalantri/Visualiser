f5f48b892c63bb4722af958cdec1591b5b36ed8a
Jayant <jayant@jivi.ai>
2024-07-17 10:25:59 +0000

    fix text plotting

--- main.py
+++ main.py
@@ -3,25 +3,33 @@ import encoder_transformers
 import encoder_open_clip
 from plotter import plotter
 
-hf_model = "hf-hub:microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224"
+hf_model = ""
 text = True
 image_folder = ""
-threeD = False
+threeD = True
 num_samples = 100
 
 categories = [x[1] for x in os.walk(image_folder)][0]
 
-t = encoder_transformers.encoder(hf_model)
+texts = ["" for _ in range(len(categories))]
+
+for i in range(len(categories)):
+    texts[i] = categories[i].replace("no pneumonia", "healthy lung")
+
+# t = encoder_transformers.encoder(hf_model)
 o = encoder_open_clip.encoder(hf_model)
 p = plotter(threeD)
 
 
 image_features = o.process_and_encode(image_folder, categories, num_samples, proj=text)
-text_features = o.get_text_features(text)
+if text:
+    text_features = o.get_text_features(texts)
+else:
+    text_features = None
 
 prefix = hf_model.replace("hf-hub:", "").replace("/", "-")
 
 fname = f"{prefix}_tsne_3d.png" if threeD else f"{prefix}_tsne.png"
 
 
-p.plot(image_features, text_features, fname, num_samples, categories)
+p.plot(image_features, text_features, fname, num_samples, texts)
